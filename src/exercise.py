4d2d414cdd58837558728bea79b567e4a291945f
BradleyH2 <hbradley@howardianprimary.co.uk>
2021-02-04 11:27:06 +0000

    Attempt 1

--- src/exercise.py
+++ src/exercise.py
@@ -1,6 +1,8 @@
 def main():
     #write your code below this line
     message = input("Write a message...")
+def user_input():
+  input_string = input('Bye')
 
 if __name__ == '__main__':
     main()
